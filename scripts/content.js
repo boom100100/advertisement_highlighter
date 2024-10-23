2458acf96f2bfab2c08bcb70d84d7e5b9bf33002
bernadette <bsd231@nyu.edu>
2024-10-23 10:21:24 -0400

    finished working draft

--- scripts/content.js
+++ scripts/content.js
@@ -1,38 +1,36 @@
+// sanity check
 console.log("i ran");
 
-// const allDivs = document.getElementsByTagName('div');
-// const allIframes = document.getElementsByTagName('iframe');
-// const elements = [];
+const regex = /[^A-z]ad/gm;
+const setElementBorder = e => {
+  if (
+    !(e.className.match(regex)
+    || e.id.match(regex))
+  ) {
+    return;
+  }
+  e.style.border = "50px solid #39ff14";
+  // TODO: add functionality for hiding instead of highlighting.
+  // Currently, changing the display to hidden changes nothing.
+  // (Expected behavior: the space remains occupied, but the ad is gone.)
+  // e.style.display = "hidden";
 
-// const setElementBorders = e => {
-//   if (
-//     e.className.match(/(-|_| ?)ad/gm)
-//     || e.id.match(/(-|_| ?)ad/gm)
-//   ) {
-//     // elements.push(e);
-//     e.style.border = "20px solid red";
-//   }
-// };
-
-// allDivs.forEach(setElementBorders);
-// allIframes.forEach(setElementBorders);
-
-
-const attributes = ["class", "id"];
-const elementToAttr = {
-  div: attributes,
-  iframe: attributes,
 };
-for (const [element, attrs] of Object.entries(elementToAttr)) {
-  
-  attrs.forEach(attr => {
-    const selected = document.querySelectorAll(`${element}[${attr}*="ad"]`);
-    if (selected.length === 0) {
-      return;
-    }
-    selected[0].style.border = "20px solid red";
-  });
-}
+
+const setElementBorders = (elementArray) => {
+  for (let i = 0; i < elementArray.length; i++)
+  {
+    setElementBorder(elementArray[i]);
+  }
+};
+
+const allDivs = document.getElementsByTagName('div');
+const allIframes = document.getElementsByTagName('iframe');
+
+setElementBorders(allDivs);
+setElementBorders(allIframes);
+
 
 // sanity check
-document.getElementsByTagName("body")[0].style = "color: green;";
+// document.getElementsByTagName("body")[0].style = "color: green;";
+console.log("i finished");
