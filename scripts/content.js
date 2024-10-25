16179d54bc2249d2440b4d172dfbefb0579dabb9
bernadette <bsd231@nyu.edu>
2024-10-24 21:13:28 -0400

    added mutation observer

--- scripts/content.js
+++ scripts/content.js
@@ -25,20 +25,38 @@ const hasMatchingAttr = (e) => {
   return false;
 };
 
+// Callback function to execute when mutations are observed
+const callback = (_mutationList, _observer) => {
+  document.querySelectorAll("body *").forEach(e => {
+    const isIndicatingAd = (
+      hasMatchingAttr(e)
+    );
+    if (!isIndicatingAd) {
+      // TODO: is there a way to check that text is part of article and not an ad? (For isChildOfArticle.)
+      // Being a descendent of an element with tag "article" is not enough, so probably not.
+      return;
+    }
+  
+    e.style.border = "20px solid #39ff14";
+    // TODO: add functionality for hiding ads instead of highlighting.
+    // Currently, changing the display to hidden changes nothing.
+    // (Expected behavior: the space remains occupied, but the ad is gone.)
+    // e.style.display = "hidden";
+  });
+};
 
-document.querySelectorAll("body *").forEach(e => {
-  const isIndicatingAd = (
-    hasMatchingAttr(e)
-  );
-  if (!isIndicatingAd) {
-    // TODO: is there a way to check that text is part of article and not an ad? (For isChildOfArticle.)
-    // Being a descendent of an element with tag "article" is not enough, so probably not.
-    return;
-  }
+const setMutationObserver = (callback) => {
+  // Select the node that will be observed for mutations
+  const targetNode = document.getElementsByTagName("body")[0];
+  
+  // Options for the observer (which mutations to observe)
+  const config = { attributes: true, childList: true, subtree: true };
+  
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(callback);
+  
+  // Start observing the target node for configured mutations
+  observer.observe(targetNode, config);
+}
 
-  e.style.border = "20px solid #39ff14";
-  // TODO: add functionality for hiding ads instead of highlighting.
-  // Currently, changing the display to hidden changes nothing.
-  // (Expected behavior: the space remains occupied, but the ad is gone.)
-  // e.style.display = "hidden";
-});
+setMutationObserver(callback);
